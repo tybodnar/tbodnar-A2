ba20cf02e894247e89ed7f404fbd0bc215f5be39
tybodnar <tybodnarcollege@gmail.com>
2024-03-19 16:07:19 -0400

    created to do app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,59 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+     import '../style.css';
+     let toDoItem = '';
+     let toDoList = [];
+
+     function addToArray() {
+          if (toDoItem == ''){
+               return;
+          }
+          // toDoList.push(toDoItem);
+          toDoList = [...toDoList, {
+               text: toDoItem,
+               done: false
+          }];
+          console.log(toDoList);
+          toDoItem = '';
+     }
+     function removeThis(index) {
+          toDoList.splice(index, 1);
+          toDoList = toDoList;
+     }
+
+</script>
+
+<p>remember to adjust favicon!</p>
+
+<h1>To Do!</h1>
+
+<form on:submit|preventDefault={addToArray}>
+     <input type="text" bind:value={toDoItem}>
+     <button type="submit">Add</button>
+</form>
+
+<ul>
+     {#each toDoList as item, index}
+          <li>
+               <input type="checkbox" bind:checked={item.done}>
+               <span class:done={item.done} >{item.text}</span>
+               <span on:click={() => removeThis(index)} class="remove" role="button" tabindex="0">&times;</span>
+          </li>
+     {/each}
+</ul>
+
+<style>
+     ul {
+          list-style: none;
+     }
+     li{
+          font-size: 1.3rem;
+     }
+     .done {
+          opacity: 25%;
+          text-decoration: line-through;
+     }
+     .remove{
+          color: darkred;
+          cursor: pointer;
+     }
+</style>

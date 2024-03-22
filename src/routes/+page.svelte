7b06d6b980029a92d46984932eb17cc80871b99c
tybodnar <tybodnarcollege@gmail.com>
2024-03-22 16:02:14 -0400

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,86 @@
 <script>
      import '../style.css';
-     let toDoItem = '';
-     let toDoList = [];
+     let todoItem = '';
+     let todoList = [];
+     $: isDone = todoList.filter(item => item.done);
 
      function addToArray() {
-          if (toDoItem == ''){
+          if (todoItem == '') {
                return;
           }
-          // toDoList.push(toDoItem);
-          toDoList = [...toDoList, {
-               text: toDoItem,
-               done: false
+          todoList = [...todoList, {
+               text: todoItem,
+               done: false,
+
           }];
-          console.log(toDoList);
-          toDoItem = '';
+          console.log(todoList);
+          todoItem = '';
      }
      function removeThis(index) {
-          toDoList.splice(index, 1);
-          toDoList = toDoList;
+          todoList.splice(index, 1);
+          todoList = todoList;
      }
-
+     function clearDone() {
+          todoList = todoList.filter(item => !item.done)
+     }
 </script>
 
-<p>remember to adjust favicon!</p>
+<h1>To Do List</h1>
 
-<h1>To Do!</h1>
-
-<form on:submit|preventDefault={addToArray}>
-     <input type="text" bind:value={toDoItem}>
-     <button type="submit">Add</button>
+<form on:submit|preventDefault={addToArray}  class="flex">
+     <input type="text" bind:value={todoItem}>
+     <div>
+          <button type="submit">Add</button>
+     </div>
 </form>
 
 <ul>
-     {#each toDoList as item, index}
+     {#each todoList as item, index}
           <li>
                <input type="checkbox" bind:checked={item.done}>
+
                <span class:done={item.done} >{item.text}</span>
                <span on:click={() => removeThis(index)} class="remove" role="button" tabindex="0">&times;</span>
           </li>
      {/each}
 </ul>
 
+{#if isDone.length > 0}
+     <button on:click={clearDone}>Remove Done</button>
+{/if}
+
 <style>
      ul {
           list-style: none;
      }
-     li{
+     li {
           font-size: 1.3rem;
      }
      .done {
           opacity: 25%;
           text-decoration: line-through;
      }
-     .remove{
+     .remove {
           color: darkred;
           cursor: pointer;
      }
+     h1{
+     text-align: center;
+     padding-top: 5vw;
+     }
+
+     .flex{
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 10px;
+     }
+
+     .center{
+          align-items: center;
+          display: flex;
+          justify-content: center;
+          gap: 10px;
+     }
+
 </style>
